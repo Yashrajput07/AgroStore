9597da41c8fa5415d190ed8730e1a102c261f06c
Prajwal Atram <91720831+prajwalatram28@users.noreply.github.com>
2024-02-21 22:05:25 +0530

    Add files via upload

---
+++ app/src/main/java/com/project/agrostore/articles/listener/ExpandClickListener.java
@@ -0,0 +1,7 @@
+package com.project.agrostore.articles.listener;
+
+import com.project.agrostore.articles.model.HowToExpandResponse;
+
+public interface ExpandClickListener {
+    void onExpandItemClick(HowToExpandResponse response);
+}
