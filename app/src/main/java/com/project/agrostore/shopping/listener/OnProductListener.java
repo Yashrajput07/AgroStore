e7ab9c642fc3b1c4998e3e77431ca822434bea91
Prajwal Atram <91720831+prajwalatram28@users.noreply.github.com>
2024-02-21 22:43:09 +0530

    Add files via upload

---
+++ app/src/main/java/com/project/agrostore/shopping/listener/OnProductListener.java
@@ -0,0 +1,7 @@
+package com.project.agrostore.shopping.listener;
+
+import com.project.agrostore.shopping.model.ProductModel;
+
+public interface OnProductListener {
+    void onProductClick(ProductModel productModel);
+}
