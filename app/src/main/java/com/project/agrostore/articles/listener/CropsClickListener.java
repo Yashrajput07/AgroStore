52972429410c324d72deb63295e55dcab170480c
Prajwal Atram <91720831+prajwalatram28@users.noreply.github.com>
2024-02-21 22:05:00 +0530

    Add files via upload

---
+++ app/src/main/java/com/project/agrostore/articles/listener/CropsClickListener.java
@@ -0,0 +1,7 @@
+package com.project.agrostore.articles.listener;
+
+import com.project.agrostore.articles.model.CropsResponse;
+
+public interface CropsClickListener {
+    void onCropsClick(CropsResponse response);
+}
