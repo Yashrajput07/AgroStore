bf60b89ced2addc576f7a5946190c884e48ff70a
Prajwal Atram <91720831+prajwalatram28@users.noreply.github.com>
2024-02-21 22:05:49 +0530

    Add files via upload

---
+++ app/src/main/java/com/project/agrostore/articles/listener/FruitsClickListener.java
@@ -0,0 +1,7 @@
+package com.project.agrostore.articles.listener;
+
+import com.project.agrostore.articles.model.FruitsResponse;
+
+public interface FruitsClickListener {
+    void onFruitClick(FruitsResponse response);
+}
