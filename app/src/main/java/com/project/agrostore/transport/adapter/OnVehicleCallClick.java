a6194e884b0568451ce2a50643dff941c27e763e
Prajwal Atram <91720831+prajwalatram28@users.noreply.github.com>
2024-02-22 12:53:34 +0530

    Create OnVehicleCallClick.java

---
+++ app/src/main/java/com/project/agrostore/transport/adapter/OnVehicleCallClick.java
@@ -0,0 +1,7 @@
+package com.project.agrostore.transport.adapter;
+
+import com.project.agrostore.transport.model.VehicleModel;
+
+public interface OnVehicleCallClick {
+    void callVehicleOwner(VehicleModel vehicleModel);
+}
